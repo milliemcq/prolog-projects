2462d9b8729152bc6fd0e29c63f7678018532429
Your Name Here <millie@mcquillin.com>
2019-01-16 12:40:33 +0000

    Explanations added for 01

--- 99PrologProblems/01_last_elem_list.pl
+++ 99PrologProblems/01_last_elem_list.pl
@@ -1,4 +1,8 @@
+%
 % Find the last element in a list
+% This works by repeatedly calling on the tail of the list
+% until there is only one element left, this element then becomes X
+%
 
 last_elem(X,[X]).
 last_elem(X,[_|L]) :- last_elem(X,L).
