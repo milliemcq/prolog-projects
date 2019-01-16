8cf1e53398d31abec56645c8beb712ddfd0febf5
Your Name Here <millie@mcquillin.com>
2019-01-16 12:42:27 +0000

    Explanations added for 02

--- 99PrologProblems/02_last_but_one_elem_list.pl
+++ 99PrologProblems/02_last_but_one_elem_list.pl
@@ -1,4 +1,9 @@
+%
 % find the last but one element of a list
+% Solution similar to first, but instead you wait till
+% only two elements remain in the list
+% and then assign X to the second of these two elements.
+%
 
 last_but_one(X,[X,_]).
 last_but_one(X,[_,Y|Ys]) :- last_but_one(X,[Y|Ys]).
