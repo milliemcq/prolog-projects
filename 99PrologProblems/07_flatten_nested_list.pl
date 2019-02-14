cdc85094620dfe283e5a6ebdb4d9d28482f1adca
Your Name Here <millie@mcquillin.com>
2019-02-14 12:56:53 +0000

    07 Added

--- 99PrologProblems/07_flatten_nested_list.pl
+++ 99PrologProblems/07_flatten_nested_list.pl
@@ -1,3 +1,8 @@
 % write a prolog program that flattens a nested list, i.e [a, b, [c, d], e] = [a, b, c, d, e]
 
-my_flattens([]).
+my_flatten(ListOut, ListIn) :- my_flatten(ListOut, ListIn, []).
+my_flatten([], Res, Res) :- !.
+my_flatten([Head|Tail], Res, Cont) :-   !,
+                                        my_flatten(Head, Res, Cont1),
+                                        my_flatten(Tail, Cont1, Cont).
+my_flatten(Term, [Term|Cont], Cont).
