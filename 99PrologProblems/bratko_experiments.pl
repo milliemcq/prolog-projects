05580ef736e398d17793f822fe24e13101fa9bca
Your Name Here <millie@mcquillin.com>
2019-03-31 07:20:37 +0100

    Odd/Even Length

--- 99PrologProblems/bratko_experiments.pl
+++ 99PrologProblems/bratko_experiments.pl
@@ -5,3 +5,11 @@ del(X,[Y|Tail],[Y|Tail1]):-
 
 % You can use delete to insert into a list like so
 % del(a, L, [1, 2, 3]).
+
+evenlength([]).
+
+evenlength([First|Rest]):-
+  oddlength(Rest)
+
+oddlength([First|Rest]):-
+  evenlength(Rest)
