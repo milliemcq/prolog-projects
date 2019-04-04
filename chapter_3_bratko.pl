95d889ec870a18700d6846780f76f4693bc4fb6a
Your Name Here <millie@mcquillin.com>
2019-04-04 08:24:34 +0100

    3.2 Complete

--- chapter_3_bratko.pl
+++ chapter_3_bratko.pl
@@ -3,6 +3,10 @@ conc([], L, L).
 conc([X|L1], L2, [X|L3]):-
   conc(L1, L2, L3).
 
-  % 3.1 - Delete the last 3 elements from a list
-  delete_last_three(L, L1):-
-      conc(L1, [_, _, _], [1, 2, 3, 4]).
+% 3.1 - Delete the last 3 elements from a list
+delete_last_three(L, L1):-
+    conc(L1, [_, _, _], [1, 2, 3, 4]).
+
+% 3.2 - Get last element from list
+last(Item, List):-
+    conc(_ , [Item], List).
