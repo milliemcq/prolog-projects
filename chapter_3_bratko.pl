b97d64c74e0bb440125465cc10a7ef383f36ca85
Your Name Here <millie@mcquillin.com>
2019-04-04 08:05:50 +0100

    Delete last three elements works

--- chapter_3_bratko.pl
+++ chapter_3_bratko.pl
@@ -1,2 +1,8 @@
-% 3.1 - Delete the last 3 elements from a list
-last_three([H|_ _ _], H).
+conc([], L, L).
+
+conc([X|L1], L2, [X|L3]):-
+  conc(L1, L2, L3).
+
+  % 3.1 - Delete the last 3 elements from a list
+  delete_last_three(L, L1):-
+      conc(L1, [_, _, _], [1, 2, 3, 4]).
