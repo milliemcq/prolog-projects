7e27c28681cf789b8497556fe58360a738e0cddd
Your Name Here <millie@mcquillin.com>
2019-04-08 08:58:56 +0100

    Even/Odd length calculator

--- chapter_3_bratko.pl
+++ chapter_3_bratko.pl
@@ -10,3 +10,13 @@ delete_last_three(L, L1):-
 % 3.2 - Get last element from list
 last(Item, List):-
     conc(_, [Item], List).
+
+% 3.3 - Find whether odd or even length List
+evenlength([]).
+
+evenlength([_|Rest]):-
+    oddlength(Rest).
+
+oddlength([_|Rest]):-
+  evenlength(Rest).
+  
