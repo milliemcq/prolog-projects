c7a826a2d27c8fd4bdcad7d47da9cb8e25341af6
Your Name Here <millie@mcquillin.com>
2019-04-22 08:28:48 +0100

    Problems 1 & 2 Complete

---
+++ 99PrologProblems/all_problems.pl
@@ -0,0 +1,11 @@
+% 01 - Find the last element in a list
+last_elem(X, [X]).
+
+last_elem(X, [_|L]):-
+  last_elem(X, L).
+
+% 02 - Find the last but one element of a List
+last_but_one(X, [X, _]).
+
+last_but_one(X, [_|L]):-
+  last_but_one(X, L).
