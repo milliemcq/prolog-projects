51251e0ee38a634fa2ccb58768445ff2f1ab59bc
Your Name Here <millie@mcquillin.com>
2019-05-06 07:34:59 +0100

    Changes

--- 99PrologProblems/all_problems.pl
+++ 99PrologProblems/all_problems.pl
@@ -65,3 +65,17 @@ encode(L1,L2) :- pack(L1,L), transform(L,L2).
 
 transform([],[]).
 transform([[X|Xs]|Ys],[[N,X]|Zs]) :- length([X|Xs],N), transform(Ys,Zs).
+
+% ?? Taking a slice of a List
+slice([X|_],1,1,[X]).
+
+slice([X|Xs],1,K,[X|Ys]):-
+    K > 1,
+    K1 is K - 1,
+    slice(Xs,1,K1,Ys).
+
+slice([_|Xs],I,K,Ys):-
+    I > 1, I1
+    is I - 1,
+    K1 is K - 1,
+    slice(Xs,I1,K1,Ys).
